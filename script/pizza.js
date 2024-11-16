603fc387afac9f8edc090afa6cffaf34fc3f1b8a
Artur8090 <artur.cheuchiev@gmail.com>
2024-11-16 17:00:56 +0100

    fixed logical issues,  id -> class

--- script/pizza.js
+++ script/pizza.js
@@ -4,7 +4,7 @@ const pizzaPrice = [100,101,102,103,104,105]
 function createPizzaElement(pizzaElementArray, pizzaPriceArray) {
     for (let i = 0; i < pizzaElementArray.length; i++) {
         menuItemContainer.innerHTML += `
-                <div class="menu-item" id='unselected-pizza'>
+                <div class="menu-item unselected-pizza">
                         <p class="pizza-name">${pizzaElementArray[i]}</p>
                             <img src="${'/img/Pizza' + (i + 1) + '.png'}" alt="${pizzaName[i]}" class="pizza-img">
                         <p class="pizza-price">${pizzaPriceArray[i]}:-</p>
--- script/selection.js
+++ script/selection.js
@@ -1,12 +1,11 @@
 const menuItems = document.getElementsByClassName('menu-item');
 
-// Convert HTMLCollection to an array using Array.from()
 Array.from(menuItems).forEach((element) => {
     element.addEventListener('click', () => {
-        if (element.id === 'unselected-pizza') { // Use === for comparison
-            element.id = 'selected-pizza';
+        if (element.classList[1] === 'unselected-pizza') {
+            element.className = 'menu-item selected-pizza';
         } else {
-            element.id = 'unselected-pizza';
+            element.className = 'menu-item unselected-pizza';
         }
     });
 });
