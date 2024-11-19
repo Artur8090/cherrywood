0ec9586b7512d5b950207665c1c825a3c26d1252
artur8090 <artur.cheuchiev@gmail.com>
2024-11-19 16:01:42 +0100

    added buttons

--- script/route.js
+++ script/route.js
@@ -1,7 +1,28 @@
+const mainHeader = document.querySelector('#menu-container-header')
 const continueButton = document.querySelector('.continue');
-
-continueButton.addEventListener('click',()=>{
-    menuContainer.style.display = 'none';
-    extrasContainer.style.display = 'flex'
+const backButton = document.querySelector('.back')
+let slideNumber = 1;
+changeSlide(slideNumber)
+continueButton.addEventListener('click', () => {
+    slideNumber++;
+    changeSlide(slideNumber)
 })
+backButton.addEventListener('click', () =>{
+    slideNumber--;
+    changeSlide(slideNumber);
+})
+function changeSlide(num) {
+    if(num == 1){
+        menuItemContainer.style.display = 'flex';
+        extrasItemContainer.style.display = 'none';
+        mainHeader.innerText = 'Menu'
+        backButton.style.display ='none';
 
+    } else if(num == 2){
+
+        menuItemContainer.style.display = 'none';
+        extrasItemContainer.style.display = 'flex';
+        mainHeader.innerText = 'Extras'
+        backButton.style.display = 'block'
+    }
+}
--- script/selection.js
+++ script/selection.js
@@ -1,16 +1,26 @@
 const menuItems = document.getElementsByClassName('menu-item');
+const extraItems = document.getElementsByClassName('extras-item')
 const menuContainer = document.querySelector('.menu-container')
 const extrasContainer = document.querySelector('.extras-container');
 const saveButton = document.querySelector('.continue');
 let selectedItems = [];
 let menuItemsArray = Array.from(menuItems);
-
+let extraItemsArray = Array.from(extraItems);
 menuItemsArray.forEach((element) => {
     element.addEventListener('click', () => {
         if (element.classList[1] === 'unselected-pizza') {
             element.className = 'menu-item selected-pizza';
         } else {
             element.className = 'menu-item unselected-pizza';
+        }
+    });
+});
+extraItemsArray.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (element.classList[1] === 'unselected-extras') {
+            element.className = 'extras-item selected-extras';
+        } else {
+            element.className = 'extras-item unselected-extras';
         }
     });
 });
@@ -21,5 +31,11 @@ saveButton.addEventListener('click',()=>{
             selectedItems.push(menuItemsArray[i].id)
         }
     }
+    for(let i = 0; i < extraItemsArray; i++){
+        if(extraItemsArray[i].classList[1] == 'selected-extras'){
+            selectedItems.push(extraItemsArray[i].id)
+        }
+    }
+    console.log(selectedItems)
 })
 
