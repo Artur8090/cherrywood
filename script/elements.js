1b89d234a743702d0856d7ad4d06855188d28f32
Artur8090 <artur.cheuchiev@gmail.com>
2024-11-20 00:14:09 +0100

    added anchor to the bottom and extra elements

--- script/elements.js
+++ script/elements.js
@@ -3,7 +3,7 @@
 const menuItemContainer = document.querySelector('.menu-item-container')
 const pizzaName = ['Pepperoni', 'Margharita', 'The Gamble', 'Hawaii', 'Tomato Empire', 'Mushroom Kingdom'];
 const pizzaPrice = [100000,101020,102324,1043324,1043244,'∞']
-const pizzaIngredients = [[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15],[16,17,18]]
+const pizzaIngredients = [['Pepperoni ','Mozarella ','Parmesan'],[4,5,6],[7,8,9],[10,11,12],[13,14,15],[16,17,18]]
 function createPizzaElement(pizzaElementArray, pizzaPriceArray) {
     for (let i = 0; i < pizzaElementArray.length; i++) {
         menuItemContainer.innerHTML += `
@@ -11,7 +11,7 @@ function createPizzaElement(pizzaElementArray, pizzaPriceArray) {
                         <p class="pizza-name">${pizzaElementArray[i]}</p>
                         <p class="pizza-ingredient">${ingredientToStr(pizzaIngredients[i])}</p>
                             <img src="${'/img/Pizza' + (i + 1) + '.png'}" alt="${pizzaName[i]}" class="pizza-img">
-                        <p class="pizza-price">${pizzaPriceArray[i]}:-</p>
+                        <p class="pizza-price">${pizzaPriceArray[i]}NOK:-</p>
                 </div>`
     }
 }
@@ -31,15 +31,15 @@ function ingredientToStr(ingArr){
 
 
 const extrasItemContainer = document.querySelector('.extras-item-container')
-const extrasName = ['Cheese','Tomatos','Mushrooms','Olives','Pepperonies','Bacon'];
-const extrasPrice = [20,20,20,20,20,20];
+const extrasName = ['Parmesan','Mozarella','Mushrooms','Olives','Pepperonies','Bacon','Bellpeppers','Tomatos'];
+const extrasPrice = [20000,20,20,20,20,20,20,20];
 function createExtrasElement(extrasElementArray, extrasPriceArray){
     for(let i = 0; i < extrasElementArray.length; i++){
         extrasItemContainer.innerHTML += `
         <div class="extras-item unselected-extras"  id='${extrasName[i].toLowerCase()}'>
                 <p class="extras-name">Extra ${extrasElementArray[i]}</p>
                     <img src="${'/img/extras' + (i + 1) + '.png'}" alt="${extrasName[i]}" class="extras-img">
-                <p class="pizza-price">${extrasPriceArray[i]}:-</p>
+                <p class="extras-price">${extrasPriceArray[i]}:-</p>
         </div>`
     }
 }
